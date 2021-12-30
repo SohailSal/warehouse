89950b4abfdfa9f2ec782ca3f5d31e52a1a42bd3
MuhammadHaris55 <hkhn567@gmail.com>
2021-12-30 12:45:57 +0500

    error msges

--- resources/js/Pages/UnitTypes/Edit.vue
+++ resources/js/Pages/UnitTypes/Edit.vue
@@ -7,6 +7,21 @@
     </template>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-4">
       <div class="">
+        <div
+          class="
+            text-center
+            bg-red-100
+            border border-red-400
+            text-red-700
+            px-4
+            py-2
+            rounded
+          "
+          role="alert"
+          v-if="isError"
+        >
+          {{ firstError }}
+        </div>
         <form @submit.prevent="submit">
           <div class="p-2 mr-2 mb-2 mt-4 ml-6 flex flex-wrap">
             <label class="my-2 mr-8 text-right w-36 font-bold"
@@ -74,6 +89,14 @@ export default {
   methods: {
     submit() {
       this.$inertia.put(route("unittypes.update", this.unittype.id), this.form);
+    },
+  },
+  watch: {
+    errors: function () {
+      if (this.errors) {
+        this.firstError = this.errors[Object.keys(this.errors)[0]];
+        this.isError = true;
+      }
     },
   },
 };
